44d838d672b115e0f7549d41a8a90d419ac3829b
Austin Cohrs <acohrsdev@outlook.com>
2017-11-13 15:49:00 -0800

    Added login view model and additional userAuth actions in the userAuth controller.

--- StickerFire/StickerFire/Controllers/UserAuthController.cs
+++ StickerFire/StickerFire/Controllers/UserAuthController.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using StickerFire.Models;
@@ -6,6 +7,7 @@ using StickerFire.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace StickerFire.Controllers
@@ -28,6 +30,101 @@ namespace StickerFire.Controllers
         public IActionResult Index(string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        //Register regular user
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel rvm, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = rvm.Email, Email = rvm.Email };
+                var result = await _userManager.CreateAsync(user, rvm.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                }
+            }
+            return View();
+        }
+
+
+        //Admin Registration form
+        [HttpGet]
+        public IActionResult RegisterAdmin(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        //Register Admin user
+        [HttpPost]
+        public async Task<IActionResult> RegisterAdmin(RegisterViewModel avm, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = avm.Email, Email = avm.Email };
+                var result = await _userManager.CreateAsync(user, avm.Password);
+
+                if (result.Succeeded)
+                {
+                    List<Claim> memberClaims = new List<Claim>();
+
+                    Claim admin = new Claim(ClaimTypes.Role, "Admin", ClaimValueTypes.String);
+                    memberClaims.Add(admin);
+
+                    var addClaims = await _userManager.AddClaimsAsync(user, memberClaims);
+
+
+                    if (addClaims.Succeeded)
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            return View();
+        }
+
+        //Login for all users
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel lvm)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(lvm.Email, lvm.Password, lvm.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    var userIdentity = new ClaimsIdentity("Registration");
+
+                    var userPrinciple = new ClaimsPrincipal(userIdentity);
+                    await HttpContext.SignInAsync(
+                        "MyCookieLogin", userPrinciple,
+                            new AuthenticationProperties
+                            {
+                                ExpiresUtc = DateTime.UtcNow.AddMinutes(45),
+                                IsPersistent = false,
+                                AllowRefresh = false
+
+                            });
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+            string error = "Unable to log you in.  Please try again or contact your system admin.";
+            ModelState.AddModelError("", error);
             return View();
         }
 
