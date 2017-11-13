d43516efb2f7dff64135b9990b40196cfb59e284
Austin Cohrs <acohrsdev@outlook.com>
2017-11-13 11:44:11 -0800

    Added Initial Identity to startup class. Added a data folder and UserDbContext. Added user model.

---
+++ StickerFire/StickerFire/Data/UserDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StickerFire.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StickerFire.Data
+{
+    public class UserDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+    }
+}
