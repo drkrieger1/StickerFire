cefda55b5007759d27ea4ff8a5bb27263b4c4c29
drkrieger <eplyushko@gmail.com>
2017-11-13 10:42:47 -0800

    base campagn class built out with properties

---
+++ StickerFire/StickerFire/Models/Campaign.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StickerFire.Models
+{
+    public class Campaign
+    {
+        int ID { get; set; }
+
+        int OwnerID { get; set; }
+        string ImgPath { get; set; }
+        string Descrition { get; set; }
+        bool Published { get; set; }
+        //enum category {get; set; }
+        int Votes { get; set; }
+        int Views { get; set; }
+        //Votes Voters 
+        string DenyMessage { get; set; }
+        bool Active { get; set; }
+        DateTime Expiration { get; set; }
+
+    }
+}
