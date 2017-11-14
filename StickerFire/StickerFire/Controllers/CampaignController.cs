0af7c16650784ee4cd230e759928a6fa0b5ef4bb
drkrieger <eplyushko@gmail.com>
2017-11-13 16:07:07 -0800

    Db context dependency injection added, index method grabs all campains and the create method was created

--- StickerFire/StickerFire/Controllers/CampaignController.cs
+++ StickerFire/StickerFire/Controllers/CampaignController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StickerFire.Data;
+using StickerFire.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,36 @@ namespace StickerFire.Controllers
 {
     public class CampaignController : Controller
     {
+        private readonly StickerFireDbContext _Context;
+
+        public CampaignController(StickerFireDbContext context)
+        {
+            _Context = context;
+        }
+
+        //Index Gathering all campaigns from the Context
+        public async Task<IActionResult> Index()
+        {
+            return View(await _Context.Campaign.ToListAsync());
+        }
+        //Get the create View
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Create([Bind("ID,OwnerID,Votes,Views,ImgPath,Description,DenyMessage,Published,Active,Category,Status")]Campaign campaign)
+        {
+            if (ModelState.IsValid)
+            {
+                _Context.Add(campaign);
+                await _Context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(campaign);
         }
     }
 }
--- StickerFire/StickerFire/Models/Campaign.cs
+++ StickerFire/StickerFire/Models/Campaign.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace StickerFire.Models
 {
     public class Campaign
-    {
+    {   
+        [Key]
         int ID { get; set; }
         int OwnerID { get; set; }
         int Votes { get; set; }
