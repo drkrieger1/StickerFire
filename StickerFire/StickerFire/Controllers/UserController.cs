5e0ca2670276be9e4ee0e57ef124afa7690dc594
Austin Cohrs <acohrsdev@outlook.com>
2017-11-13 19:10:29 -0800

    Added admin only policy , user controller, and user view index.

---
+++ StickerFire/StickerFire/Controllers/UserController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StickerFire.Data;
+using StickerFire.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StickerFire.Controllers
+{
+   // [Authorize(Policy = "Admin Only")]
+    public class UserController : Controller
+    {
+        private readonly UserDbContext _context;
+
+        public UserController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ApplicationUser.ToListAsync());
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var theUsers = await _context.ApplicationUser
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (theUsers == null)
+            {
+                return NotFound();
+            }
+
+            return View(theUsers);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var theUsers = await _context.ApplicationUser.SingleOrDefaultAsync(m => m.Id == id);
+            if (theUsers == null)
+            {
+                return NotFound();
+            }
+            return View(theUsers);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] ApplicationUser theUsers)
+        {
+            if (id != theUsers.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(theUsers);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TheUserExists(theUsers.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(theUsers);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var crewMember = await _context.ApplicationUser
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (crewMember == null)
+            {
+                return NotFound();
+            }
+
+            return View(crewMember);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var theUser = await _context.ApplicationUser.SingleOrDefaultAsync(m => m.Id == id);
+            _context.ApplicationUser.Remove(theUser);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TheUserExists(string id)
+        {
+            return _context.ApplicationUser.Any(e => e.Id == id);
+        }
+    }
+}
